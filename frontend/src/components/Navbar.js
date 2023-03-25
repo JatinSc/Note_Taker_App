4eb3c8f879f4fb6eed5c7759649d43f91101794a
jatinSc <jatin.singhchauhan.4@gmail.com>
2023-03-25 17:14:46 +0530

    minor changes done

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -40,8 +40,9 @@ const Navbar = ({ title = "Note Taker App" }) => {
             </svg>
             <a className="navbar-brand"><strong>{title}</strong></a>
           </Link>
-
-
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <div className="collapse navbar-collapse" id="navbarColor01">
             <ul className="navbar-nav ms-auto">
               {user ? <>
